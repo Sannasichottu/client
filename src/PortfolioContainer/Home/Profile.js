dd8508f6f09e7593ec6f2a2332ef8b630af6af2a
sannasichottu <sannasichottu@gmail.com>
2022-02-15 23:12:51 +0530

    home header and mobile responsive

--- src/PortfolioContainer/Home/Profile.js
+++ src/PortfolioContainer/Home/Profile.js
@@ -1,36 +1,39 @@
-import React from 'react';
+import React from "react";
 //import Typical from 'typical';
+import "./Profile.css";
 
 export default function Profile() {
   return (
-    <div className='profile-container'>
-        <div className='profile-parent'>
-            <div className='profile-details'>
-                <div className='colz'>
-                    <a href='https://www.facebook.com/profile.php?id=100008169066805'>
-                        <i className='fa fa-facebook-square'></i>
-                    </a>
-                    <a href='https://www.instagram.com/sannasi_chotu/'>
-                        <i className='fa fa-instagram'></i>
-                    </a>
-                    <a href='https://twitter.com/ChottuSannasi'>
-                        <i className='fa fa-twitter'></i>
-                    </a>
-                    <a href='https://www.linkedin.com/in/sannasi-m-58a9b421a/'>
-                        <i className='fa fa-linkedin-square'></i>
-                    </a>
-                    <a href='https://github.com/Sannasichottu'>
-                        <i className='fa fa-github-square'></i>
-                    </a>
-                </div>
+    <div className="profile-container">
+      <div className="profile-parent">
+        <div className="profile-details">
+          <div className="colz">
+            <div className="colz-icon">
+              <a href="https://www.facebook.com/profile.php?id=100008169066805">
+                <i className="fa fa-facebook-square"></i>
+              </a>
+              <a href="https://www.instagram.com/sannasi_chotu/">
+                <i className="fa fa-instagram"></i>
+              </a>
+              <a href="https://twitter.com/ChottuSannasi">
+                <i className="fa fa-twitter"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/sannasi-m-58a9b421a/">
+                <i className="fa fa-linkedin-square"></i>
+              </a>
+              <a href="https://github.com/Sannasichottu">
+                <i className="fa fa-github-square"></i>
+              </a>
+            </div>
+          </div>
 
-                <div className="profile-details-name">
-                    <span className="primary-text">
-                        {" "}
-                        Hello, I'M <span className="highlighted-text">Sannasi</span>
-                    </span>
-                </div>
-              {/*   <div className='profile-details-role'>
+          <div className="profile-details-name">
+            <span className="primary-text">
+              {" "}
+              Hello, I'M <span className="highlighted-text">Sannasi</span>
+            </span>
+          </div>
+          {/*   <div className='profile-details-role'>
                     <span className='primary-text'>
                         {" "}
                         <h1>
@@ -56,17 +59,20 @@ export default function Profile() {
                         </span>
                     </span>
                 </div>*/}
-                <div className='profile-options'>
-                    <button className='btn primary-btn'>
-                        {""}
-                        Hire Me{" "}
-                    </button>
-                    <a href='Sannasicv.pdf' download='Sannasi FSD cv.pdf'>
-                        <button className='btn highlighted-btn'>Get Resume</button>
-                    </a>
-                </div>
-            </div>
-        </div> 
+          <div className="profile-options">
+            <button className="btn primary-btn">
+              {""}
+              Hire Me{" "}
+            </button>
+            <a href="Sannasicv.pdf" download="Sannasi FSD cv.pdf">
+              <button className="btn highlighted-btn">Get Resume</button>
+            </a>
+          </div>
+        </div>
+        <div className="profile-picture">
+          <div className="profile-picture-background"></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
